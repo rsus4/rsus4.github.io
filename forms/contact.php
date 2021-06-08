567336b73a7a66aad1ccd9163af6150c46b139cf
rsus4 <59598251+rsus4@users.noreply.github.com>
2021-06-09 04:03:44 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'rishit19091@iiitd.ac.in';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
